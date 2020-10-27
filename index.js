44c94ed1c0fca33de82187c7051646c55bb96cf1
Gavrilov Alexandr <@gavrilov.alex.official>
2020-10-27 16:09:26 +0200

    fix: try fix cors

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(cors({
   preflightContinue: true,
 }))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use('/api/user', userRoute);
 
 app.use(express.static('client/build'));
